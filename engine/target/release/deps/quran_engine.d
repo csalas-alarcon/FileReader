27d38f01146c9b93424efee181d1740f5c869b26
csalas-alarcon <csalas0821@gmail.com>
2026-01-01 20:20:15 +0100

    Repurpouse of the Repo, Welcome TO RUST

--- engine/target/release/deps/quran_engine.d
+++
@@ -1,5 +0,0 @@
-/home/csalas/Desktop/FileReader/engine/target/release/deps/quran_engine.d: src/lib.rs
-
-/home/csalas/Desktop/FileReader/engine/target/release/deps/libquran_engine.so: src/lib.rs
-
-src/lib.rs:
--- engine/target/release/libquran_engine.d
+++
@@ -1,1 +0,0 @@
-/home/csalas/Desktop/FileReader/engine/target/release/libquran_engine.so: /home/csalas/Desktop/FileReader/engine/src/lib.rs
